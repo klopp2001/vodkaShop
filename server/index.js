367938b84cf87aa0f406ab2641e1ce21c851ed37
KapralDaun <klad002@mail.ru>
2024-08-01 23:25:11 +0300

    fck

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 
 app.listen(port, (req,res) => {
   console.log(`server is running on d port ${port}`)
 })
 app.use(express.json())
 app.use(cors())
-
+app.get("/", (req, res) =>{
+  res.send("Hello")
+})
 console.log('Гойда')
